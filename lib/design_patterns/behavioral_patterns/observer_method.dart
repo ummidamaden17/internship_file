1eb16e0e2d578ce6f26ac2f962f9b3797eb974ac
Admin <madenumida@gmail.com>
2025-03-05 06:41:57 +0500

    observer_method

--- lib/design_patterns/behavioral_patterns/observer_method.dart
+++ lib/design_patterns/behavioral_patterns/observer_method.dart
@@ -1,1 +1,39 @@
+abstract class EventListener {
+  void update(String filename);
+}
 
+class EventManager {
+  final Map<String, List<EventListener>> _listeners = {};
+
+  void subscribe(String eventType, EventListener listener) {
+    _listeners.putIfAbsent(eventType, () => []).add(listener);
+  }
+
+  void unsubscribe(String eventType, EventListener listener) {
+    _listeners[eventType]?.remove(listener);
+  }
+
+  void notify(String eventType, String data) {
+    if (_listeners.containsKey(eventType)) {
+      for (var listener in _listeners[eventType]!) {
+        listener.update(data);
+      }
+    }
+  }
+}
+
+class Editor {
+  final EventManager events = EventManager();
+  String? file;
+
+  void openFile(String path) {
+    file = path;
+    events.notify('open', file!);
+  }
+
+  void saveFile() {
+    if (file != null) {
+      events.notify('save', file!);
+    }
+  }
+}
